5634272c2229bb6fd913d7d88ccbc27b46a721d8
janiyasir <167489014+janiyasir@users.noreply.github.com>
2024-06-28 05:30:23 +0500

    Update app.py

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://facebook_blindcity:59257e84d141
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
